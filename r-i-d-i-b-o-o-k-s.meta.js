f28f03aa9d83c1d008cdd9848416546a0165ca67
ldocultsen <ldocultsen@github.com>
2024-08-14 23:44:02 +0700

    init

---
+++ r-i-d-i-b-o-o-k-s.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         r-i-d-i-b-o-o-k-s
+// @namespace    https://github.com/ldocultsen/bai_lang_ra/raw/main/r-i-d-i-b-o-o-k-s.user.js
+// @version      2024.08.14
+// @description  down r-i-d-i-b-o-o-k-s
+// @author       You
+// @include      /^https:\/\/r[i1]dib[o0]oks.c[o0]m\/b[o0]oks\/.+?\/vi[e3]w.+/
+// @icon         https://raw.githubusercontent.com/ldocultsen/bai_lang_ra/main/r-i-d-i-b-o-o-k-s.png
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/ldocultsen/bai_lang_ra/raw/main/r-i-d-i-b-o-o-k-s.user.js
+// @updateURL    https://github.com/ldocultsen/bai_lang_ra/raw/main/r-i-d-i-b-o-o-k-s.meta.js
+// @run-at       document-start
+// ==/UserScript==
