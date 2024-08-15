54a182d74b0cb33d282345f979b4308910b4f30c
ldocultsen <ldocultsen@github.com>
2024-08-15 21:02:26 +0700

    up

--- r-i-d-i-b-o-o-k-s.meta.js
+++ r-i-d-i-b-o-o-k-s.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         r-i-d-i-b-o-o-k-s
 // @namespace    https://github.com/ldocultsen/bai_lang_ra/raw/main/r-i-d-i-b-o-o-k-s.user.js
-// @version      2024.08.14
+// @version      2024.08.15
 // @description  down r-i-d-i-b-o-o-k-s
 // @author       You
-// @include      /^https:\/\/r[i1]dib[o0]oks.c[o0]m\/b[o0]oks\/.+?\/vi[e3]w.+/
+// @include      /^https:\/\/r[i1]dib[o0]oks.c[o0]m\/b[o0]oks\/.+?\//
 // @icon         https://raw.githubusercontent.com/ldocultsen/bai_lang_ra/main/r-i-d-i-b-o-o-k-s.png
 // @grant        GM_download
 // @grant        GM_info
